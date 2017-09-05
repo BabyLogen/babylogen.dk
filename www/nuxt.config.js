45335d6ecd6d9c1f5fa53a9b22c79646851ce8c5
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2017-09-05 21:21:13 +0200

    Reintroduce experiements

--- www/nuxt.config.js
+++ www/nuxt.config.js
@@ -56,7 +56,7 @@ module.exports = {
   ],
   plugins: [
     { src: '~/plugins/detect-touch', ssr: false },
-    // { src: '~/plugins/experiments', ssr: false },
+    { src: '~/plugins/experiments', ssr: false },
   ],
   generate: {
     routes() {
