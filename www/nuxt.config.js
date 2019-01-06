83eba54cb420abaa97bb892c45454ce1a9c63413
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2019-01-06 08:55:06 +0100

    Ignore eslint

--- www/nuxt.config.js
+++ www/nuxt.config.js
@@ -46,16 +46,6 @@ module.exports = {
       'marked',
       './elements/GridColumn.vue',
     ],
-    extend(config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/,
-        });
-      }
-    },
   },
   manifest: {
     name: 'BabyTeamet',
